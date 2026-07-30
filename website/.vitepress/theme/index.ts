b2dc3b6cd717c91ccee747eb403f07ef2b92e5a5
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2026-07-30 09:22:31 -0400

    feat: add logo to website header

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,6 +1,20 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import { withBase } from 'vitepress'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      'nav-bar-title-before': () =>
+        h('img', {
+          class: 'site-header-logo',
+          src: withBase('/favicon.svg'),
+          alt: 'Branch Schematic logo',
+          width: 24,
+          height: 24,
+        }),
+    })
+  },
 }
